841f1867614b35f65463b31d11e6f94aed664fae
Lionel Montrieux <lionel@nii.ac.jp>
2016-03-10 13:27:46 +0900

    Spring boot application import
    Generated by spring initializr

---
+++ src/main/java/jp/ac/nii/prl/mape/plan/MapeAutoscalingPlanApplication.java
@@ -0,0 +1,12 @@
+package jp.ac.nii.prl.mape.plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeAutoscalingPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeAutoscalingPlanApplication.class, args);
+	}
+}
