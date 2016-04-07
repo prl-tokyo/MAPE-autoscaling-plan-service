ee57afa9e9cf85c8098d535b44dbd6ba413e3158
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-07 18:50:32 +0900

    InstanceType list in Deployment
    Incl. getters and setters

--- src/main/java/jp/ac/nii/prl/mape/plan/model/Deployment.java
+++ src/main/java/jp/ac/nii/prl/mape/plan/model/Deployment.java
@@ -24,26 +24,45 @@ public class Deployment {
 	@OneToMany(mappedBy="deployment")
 	private List<Instance> vms;
 	
+	@OneToMany(mappedBy="deployment")
+	private List<InstanceType> instanceTypes;
+	
+	public Adaptation getAdaptation() {
+		return adaptation;
+	}
+
 	public Integer getId() {
 		return id;
 	}
 
+	public List<InstanceType> getInstanceTypes() {
+		return instanceTypes;
+	}
+
 	public List<Instance> getVms() {
 		return vms;
+	}
+
+	public void setAdaptation(Adaptation adaptation) {
+		this.adaptation = adaptation;
 	}
 	
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
+	public void setInstanceTypes(List<InstanceType> instanceTypes) {
+		this.instanceTypes = instanceTypes;
+	}
+
 	public void setVms(List<Instance> vms) {
 		this.vms = vms;
 	}
-
+	
 	public int size() {
 		return vms.size();
 	}
-	
+
 	public String toString() {
 		return String.format("Deployment %d with %d virtual machines", id, vms.size());
 	}
