f079c320f6cde315f4558c041a9ac3d2c07812b0
Lionel Montrieux <lionel@nii.ac.jp>
2016-04-07 17:13:50 +0900

    Repository for InstanceType

---
+++ src/main/java/jp/ac/nii/prl/mape/plan/repository/InstanceTypeRepository.java
@@ -0,0 +1,9 @@
+package jp.ac.nii.prl.mape.plan.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.ac.nii.prl.mape.plan.model.InstanceType;
+
+public interface InstanceTypeRepository extends JpaRepository<InstanceType, Integer>{
+
+}
